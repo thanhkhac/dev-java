6b75445df017acf59eff578baa3e11bec719f5b7
Thanh Nguyen Khac <thanhcqb2048@gmail.com>
2024-04-02 11:53:26 +0700

    Syntax & Bitwise

---
+++ java-01-syntax/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,14 @@
+
+B
+src/main/Main.java,f\e\fe03b4fd3afe162c6e01309b98a22588e093d274
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+Å
+Q.idea/sonarlint/securityhotspotstore/f/e/fe03b4fd3afe162c6e01309b98a22588e093d274,d\a\da101d83b6b65667373de548a9f5004071396234
+B
+java-01-syntax.iml,f\7\f795cd49c9488ec3f6cf954dc7f076bd4d245eee
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
---
+++ java-01-syntax/src/main/Main.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Main {
+    /*
+     * Ôn lại về đơn vị bộ nhớ
+     * Bit(b): biểu diễn dưới dạng 1010101
+     * Byte(B): 1 byte = 8 bit
+     * Kilobyte(KB): 1KB = 1024 byte (2^10 byte)
+     * Megabyte(MB): 1mb = 1024 KB (2^20 byte)
+     * Gigabyte(GB): 1GB = 1024 MB (2^30 byte)
+     * Terabyte(TB): 1TB = 1024 GB (2^40 byte)
+     * Petabyte(PB): 1PB = 1024 TB (2^50 byte)
+     * */
+
+//        byte: Kích thước là 1 byte (8 bits), có giá trị từ -128 đến 127.
+//        short: Kích thước là 2 byte (16 bits), có giá trị từ -32,768 đến 32,767.
+//        int: Kích thước là 4 byte (32 bits), có giá trị từ -2^31 đến (2^31 - 1).
+//        long: Kích thước là 8 byte (64 bits), có giá trị từ -2^63 đến (2^63 - 1).
+//        float: Kích thước là 4 byte (32 bits), có khoảng 6-7 chữ số thập phân với độ chính xác.
+//        double: Kích thước là 8 byte (64 bits), có khoảng 15 chữ số thập phân với độ chính xác.
+//        char: Kích thước là 2 byte (16 bits), biểu diễn một ký tự Unicode và có giá trị từ 0 đến 65,535.
+
+
+    public static void main(String[] args) {
+        //Toán tử
+        System.out.println(7 >> 2);
+        //0111
+        //Dịch sang phải 2 => 0001
+        System.out.println(7 << 2);
+        //0111
+        //Dịch sang trái 2 => 1100
+        ArrayList<String> arr= new ArrayList<>();
+
+        //Toán tử bitwise
+        //^ XOR: Trả về đúng khi 2 vế khác nhau
+        System.out.println(true ^ true);
+
+        //& AND: Trả về đúng khi 2 vế cùng trả về true
+        System.out.println(true & true);
+
+        System.out.println(~1);
+    }
+}
